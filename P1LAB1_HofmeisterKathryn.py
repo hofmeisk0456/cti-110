c1d8c841337f439b3bf2d5d3ab0e1a0ea4a9e72e
hofmeisk0456 <hofmeisk0456@student.faytechcc.edu>
2025-06-12 12:55:45 -0400

    Add files via upload

---
+++ P1LAB1_HofmeisterKathryn.py
@@ -0,0 +1,9 @@
+# Kathryn Hofmeister
+# 6/11/25
+# P1LAB1
+# This program asks for the user's first and last name, then displays a welcome message.
+
+first_name = input("Please enter your first name: ")
+last_name = input("Please enter your last name: ")
+
+print("Welcome to the course,", first_name, last_name + "!")
